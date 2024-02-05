2ae132a984c0cab38c6624b6f36a6f6871f1f23c
Trainjumper1 <trainjumperx@gmail.com>
2024-02-05 22:50:30 +1000

    Create basic hyper web server
    Co-authored-by: Sky <SkyDev125@users.noreply.github.com>

---
+++ src/services/hello.rs
@@ -0,0 +1,9 @@
+use std::convert::Infallible;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::{Request, Response};
+
+pub async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
