20089f3d30b49fb25450ce9269737c19f3064999
Trainjumper1 <trainjumperx@gmail.com>
2024-02-14 00:10:15 +1000

    feat: add secret number generation to guessing game

--- training/chapter-2/guessing_game/src/main.rs
+++ training/chapter-2/guessing_game/src/main.rs
@@ -1,15 +1,20 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin()
+    let chars_read = io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+    println!("You read: {} characters", chars_read);
+    println!("The secret number was: {}", secret_number);
 }
